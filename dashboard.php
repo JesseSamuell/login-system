95903bd8fcb1dce026bef6f28e3c561d0e12c3b2
JesseSamuell <jesse.obuki@strathmore.edu>
2024-07-08 11:33:03 +0300

    demo

--- dashboard.php
+++ dashboard.php
@@ -14,22 +14,25 @@ if (!isset($_SESSION['user'])) {
 </head>
     <body>
         <header>
+            <ul>
             <nav class="menu">
             <a href="#"> <!--Home-->
-            <i class="fa fa-home"></i>Home
+            <i class="fa fa-home"></i>Home;
          </a>
             
            <a href="report.html"> <!--report-->
-            <i class="fa fa-home"></i>Report
+            <i class="fa fa-home"></i>Report;
            </a>
 
 
-           <a href="flood.html"> <!--Home-->
-            <i class="fa fa-home"></i>Flood Assessment
+           <a href="flood.html"> <!--Flood assessment-->
+            <i class="fa fa-home"></i>Flood Assessment;
            </a>
+         </ul>
         </nav>
-</header>
+        </header>
 
+<main>
         <section>
             <div class="Dashboard">
                 <h1> Welcome to your Home Page! <?php echo ''. $_SESSION['user']; ?> </h1> 
@@ -37,6 +40,8 @@ if (!isset($_SESSION['user'])) {
                     <p> We are dedicated to Forever pushing the frontier in early flood warning and reporting, to insure both you safety and peace of mind<br>
                     We value your trust in us and hope to fulfill all your needs </P> 
 </div>
+</main>
+</body>
 
 
 
